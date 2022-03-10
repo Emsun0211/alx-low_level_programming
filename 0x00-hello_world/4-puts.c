5ee235621b47dae3bcb5c356ce401a6a55d65a8c
Emsun <emsunconcept@gmail>
2022-03-10 14:44:30 -0800

    now

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,10 @@
 
 /**
  * main = entry point
+ * int = indicates the starting point
  * this programme print "Programming is like building a multilingual puzzle"
  * return (0) success
+ * return (0) for main
  **/
 
 int main(void)
