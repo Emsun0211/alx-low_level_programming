cc0126c2a0e1bdef03b142850efc73a9375c9c46
Emsun <emsunconcept@gmail>
2022-03-10 13:00:29 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 
 /**
+ * 	main - entry point
+ * 	Return (0) success
+ **/
+
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0)
+}
