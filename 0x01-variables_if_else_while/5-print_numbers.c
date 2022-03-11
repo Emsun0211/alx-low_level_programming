7cc5908efd488dc7c72e0973f4f55e9e348fabec
Emsun <emsunconcept@gmail>
2022-03-11 09:57:02 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this code prints the digit number from 0 to 10
+ * with a new line
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+	{
+		printf('%d\n', a);
+	}
+	return (0);
+}
