e72ce3d1f71fc34f2517cd0939472101d4a3c2ad
Emsun <emsunconcept@gmail>
2022-03-10 16:36:46 -0800

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * 	main- entry point
- * 	Return: 0 success
+ * main- entry point
+ * Return: 0 success
  **/
 
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-
-	return (0);
+printf("%s", "with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
