cf5d36276734869437cfcb44c3bebe4080dec502
Emsun <emsunconcept@gmail>
2022-03-15 09:21:04 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10(void) - prints alphabets 10x
+ *
+ * Return: void
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int num = 0;
+	while (num <= 10)
+	{
+		char ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+		}
+		ch++;
+	}
+	num++;
+}
