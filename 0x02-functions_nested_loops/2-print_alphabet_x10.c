e1d36093a1b258c11bdf5090ea3c1b16557294ae
Emsun <emsunconcept@gmail>
2022-03-15 09:34:10 -0700

    main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10(void) - prints alphabets 10x
+ * print_alphabet_x10 - prints alphabets 10x
  *
  * Return: void
  *
  */
 
-void print_alphabet_x10(void)
+void print_alphabet_x10 (void)
 {
 	int num = 0;
-	while (num <= 10)
+	while (num < 10)
 	{
 		char ch = 'a';
 		while (ch <= 'z')
