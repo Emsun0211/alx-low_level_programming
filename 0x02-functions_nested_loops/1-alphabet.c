25e442b9c561b4e7c87b8834e7055802c00e5709
Emsun <emsunconcept@gmail>
2022-03-15 08:08:46 -0700

    main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * This programme prints the alphabet in the lowercase
+ * Return : Always (0) success
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+	{
+		for (ch = a; ch < z; ch++)
+		{
+			_putchar('ch');
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
