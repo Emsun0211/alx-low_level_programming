1ef9196a320097d00a7f3f5780b0c3aa79fa130e
Emsun <emsunconcept@gmail>
2022-03-17 04:26:22 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -12,11 +12,12 @@ int _isupper(int c)
 
 	if (c >= 'A' && c <= 'Z')
 	{
-		_putchar('1');
+		_putchar(1);
 	}
 	else
 	{
-		_putchar('0');
+		_putchar(0);
 	}
-	_putchar ('\n');
+	_putchar('\n');
+	return (0);
 }
