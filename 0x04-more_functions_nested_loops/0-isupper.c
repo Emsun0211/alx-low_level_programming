6c2df1298c1a956de8c97e91d5c96925c58c7191
Emsun <emsunconcept@gmail>
2022-03-17 04:20:24 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include<main.h>
+
+/**
+ * _isupper - check if a character is an upper case
+ *
+ * return: 0 Always
+ **/
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+	_putchar ('\n');
+}
