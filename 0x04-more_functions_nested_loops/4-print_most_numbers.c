9bae8c7a4ac78551de82ccc87e29d66548bc4d2a
Emsun <emsunconcept@gmail>
2022-03-17 15:11:50 -0700

    prints

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,17 +6,13 @@
  * followed by a new line.
  * Return: Always 0
  */
-
-
 void print_most_numbers(void)
 {
 	int i = '0';
 
-	for (i = '0'; i <= '9'; i++)
+	for (; i <= '9'; i++)
 	{
-
-		if (i != '2' || i != '4')
-
+		if (i != '2' && i != '4')
 			_putchar(i);
 	}
 	_putchar('\n');
