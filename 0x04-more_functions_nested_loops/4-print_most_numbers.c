679e03a84c5af86155e5851f29bbb92099c9243d
Emsun <emsunconcept@gmail>
2022-03-17 15:02:31 -0700

    prints

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,11 +14,10 @@ void print_most_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		_putchar('i');
 
-		if (i == 2 || i == 4)
-			
-			break;
+		if (i != 2 || i != 4)
+
+			_putchar('i');
 	}
 	_putchar('\n');
 }
