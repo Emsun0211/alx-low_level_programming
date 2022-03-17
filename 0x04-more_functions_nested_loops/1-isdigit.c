22d0fb157380b0f06996689e5fbb52e3c81dcb96
Emsun <emsunconcept@gmail>
2022-03-17 14:24:22 -0700

    isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,11 @@
 
 /**
  * _isdigit - this programme check for digit
+ *
  * @c:  is an input integer
+ *
  * Return: returns 1 if it is an integer and returns 0 otherwise
- */
+ **/
 
 int _isdigit(int c)
 {
