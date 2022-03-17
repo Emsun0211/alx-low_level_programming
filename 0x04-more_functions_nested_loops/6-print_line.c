b7d56db6ceeaff204571dd42b196a05a9a66d722
Emsun <emsunconcept@gmail>
2022-03-17 15:28:59 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - a function that draws a straight line in the terminal.
+* followed by a new line.
+* @n: An input integer
+* Return: Always 0
+*/
+
+void print_line(int n)
+{
+	if (n > 0)
+		_putchar('_\n');
+
+	else if (n <= 0)
+
+		_putchar('\n');
+}
