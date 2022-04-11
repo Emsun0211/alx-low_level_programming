b54aa01560a0c4be669659bc6cffdcacb8fb216e
Emsun <emsunconcept@gmail>
2022-04-11 13:48:04 -0700

    my preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of file from which it was printed from
+ * return (0)
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+this programme deals with preprocessor
